2d8ddc6bfcddf826628489974306cefe8567c4f0
slabwu <134193743+slabwu@users.noreply.github.com>
2024-04-20 18:47:23 +0800

    Change text colour based on background

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ function displayOnScreen(book) {
     const card = document.createElement('div');
     card.classList.add('card');
     card.style.backgroundColor = `${book.colour}`;
+    card.style.color = `${invertColour(book.colour)}`;
 
     const remove = document.createElement('button');
     remove.innerText = `x`;
@@ -78,6 +79,25 @@ function deleteBook(bookIndex, card) {
         myLibrary[i].index--;
     }
     card.remove();
+}
+
+function invertColour(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = parseInt(hex.slice(0, 2), 16),
+        g = parseInt(hex.slice(2, 4), 16),
+        b = parseInt(hex.slice(4, 6), 16);
+    return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+        ? '#000000'
+        : '#FFFFFF';
 }
 
 

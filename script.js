08e68ae537f0062fcc630d7f78fb25d52eb6a89f
slabwu <134193743+slabwu@users.noreply.github.com>
2024-04-20 13:18:21 +0800

    Add displayOnScreen() function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const shelf = document.querySelector('.shelf');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,10 +11,20 @@ function Book(title, author, pages, read) {
     `${this.title} by ${this.author}, ${this.pages}, not read yet` ;
     }
     addBookToLibrary(this);
+    displayOnScreen(this);
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function displayOnScreen(book) {
+    const newDiv = document.createElement('div');
+    console.log(shelf);
+    console.log(newDiv);
+    newDiv.innerText = `${book.info()}`;
+    console.log(newDiv);
+    shelf.appendChild(newDiv);
 }
 
 const cat = new Book('cat', 'mr cat', 100, true);
